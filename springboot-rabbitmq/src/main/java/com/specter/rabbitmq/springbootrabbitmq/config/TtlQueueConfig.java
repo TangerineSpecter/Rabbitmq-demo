892be9268eccc4d3a586101a985c4066cea695e7
TangerineSpecter <993033472@qq.com>
2021-07-24 15:02:42 +0800

    ttl队列延迟推送演示

---
+++ springboot-rabbitmq/src/main/java/com/specter/rabbitmq/springbootrabbitmq/config/TtlQueueConfig.java
@@ -0,0 +1,89 @@
+package com.specter.rabbitmq.springbootrabbitmq.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * TTL 队列 配置文件类
+ */
+@Configuration
+public class TtlQueueConfig {
+
+    //普通交换机的名称
+    private static final String X_EXCHANGE = "X";
+    //死信交换机的名称
+    private static final String Y_DEAD_LETTER_EXCHANGE = "Y";
+    //普通队列的名称
+    private static final String QUEUE_A = "QA";
+    private static final String QUEUE_B = "QB";
+    //死信队列的名称
+    private static final String DEAD_LETTER_QUEUE = "QD";
+
+    //声明x交换机
+    @Bean("xExchange")
+    public DirectExchange xExchange() {
+        return new DirectExchange(X_EXCHANGE);
+    }
+
+    //声明y交换机
+    @Bean("yExchange")
+    public DirectExchange yExchange() {
+        return new DirectExchange(Y_DEAD_LETTER_EXCHANGE);
+    }
+
+    //声明队列 TTL 过期时间10S
+    @Bean("queueA")
+    public Queue queueA() {
+        Map<String, Object> arguments = new HashMap<>(3);
+        //设置死信交换机
+        arguments.put("x-dead-letter-exchange", Y_DEAD_LETTER_EXCHANGE);
+        //设置死信RoutingKey
+        arguments.put("x-dead-letter-routing-key", "YD");
+        //设置TTL 单位是ms
+        arguments.put("x-message-ttl", 10 * 1000);
+        return QueueBuilder.durable(QUEUE_A).withArguments(arguments).build();
+    }
+
+    //声明队列 TTL 过期时间40S
+    @Bean("queueB")
+    public Queue queueB() {
+        Map<String, Object> arguments = new HashMap<>(3);
+        //设置死信交换机
+        arguments.put("x-dead-letter-exchange", Y_DEAD_LETTER_EXCHANGE);
+        //设置死信RoutingKey
+        arguments.put("x-dead-letter-routing-key", "YD");
+        //设置TTL 单位是ms
+        arguments.put("x-message-ttl", 40 * 1000);
+        return QueueBuilder.durable(QUEUE_B).withArguments(arguments).build();
+    }
+
+    //死信队列
+    @Bean("queueD")
+    public Queue queueD() {
+        return QueueBuilder.durable(DEAD_LETTER_QUEUE).build();
+    }
+
+    //队列绑定
+    @Bean
+    public Binding queueABindingX(@Qualifier("queueA") Queue queueA,
+                                  @Qualifier("xExchange") DirectExchange xExchange) {
+        return BindingBuilder.bind(queueA).to(xExchange).with("XA");
+    }
+
+    @Bean
+    public Binding queueBBindingX(@Qualifier("queueB") Queue queueB,
+                                  @Qualifier("xExchange") DirectExchange xExchange) {
+        return BindingBuilder.bind(queueB).to(xExchange).with("XB");
+    }
+
+    @Bean
+    public Binding queueDBindingY(@Qualifier("queueD") Queue queueD,
+                                  @Qualifier("yExchange") DirectExchange yExchange) {
+        return BindingBuilder.bind(queueD).to(yExchange).with("YD");
+    }
+}
